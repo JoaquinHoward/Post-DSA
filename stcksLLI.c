94a03d3426dc5f8bb3f7170834db0b28494c7819
JoaquinHoward <falarjoaquinhoward@gmail.com>
2026-01-09 09:41:08 +0800

    Finalized the stacks code in linked list implementation.

--- stcksLLI.c
+++ stcksLLI.c
@@ -26,7 +26,29 @@ void popElem(stack* S, int data);
 
 
 int main(void){
-    printf("%d", 5);
+    printf("\nStart of program\n");
+    stack S;
+    initStack(&S);
+    push(&S, 1);
+    push(&S, 2);
+    push(&S, 3);
+    pushUnique(&S, 1);
+    pushUnique(&S, 2);
+    pushUnique(&S, 3);
+    pushUnique(&S, 4);
+    pushFirst(&S, 1);
+    pushFirst(&S, 2);
+    pushFirst(&S, 3);
+    display(S);
+    pop(&S);
+    display(S);
+    pop(&S);
+    display(S);
+    popElem(&S, 10);
+    popElem(&S, 1);
+    popElem(&S, 1);
+    display(S);
+    printf("\nEnd of program\n");
 }
 
 void initStack(stack* S){
@@ -38,8 +60,9 @@ bool isEmpty(stack S){
 }
 
 void display(stack S){
+    printf("\ndisplay:\n");
     for(nodePtr trav = S; trav!=NULL; trav=trav->link){
-        printf("%d", trav->data);
+        printf("%d ", trav->data);
     }
 }
 
@@ -49,13 +72,16 @@ void push(stack* S, int data){
         newNode->data = data;
         newNode->link = *S;
         *S = newNode;
+        printf("\nSuccessful insertion of %d.\n", data);
+    }else{
+        printf("\nFailed to insert element %d.\n", data);
     }
 }
 
 void pushUnique(stack* S, int data){
     nodePtr trav;
     for(trav = *S; trav!=NULL && trav->data!=data; trav=trav->link){}
-    if(trav!=NULL){
+    if(trav==NULL){
         nodePtr newNode = (nodePtr) malloc (sizeof(struct node));
         if(newNode!=NULL){
             newNode->data = data;
@@ -72,16 +98,15 @@ void pushUnique(stack* S, int data){
 
 void pushFirst(stack* S, int data){
     nodePtr* trav;
-    for(trav=S; *trav!=NULL; trav=&(*trav)->link){
-        nodePtr newNode = (nodePtr) malloc (sizeof(struct node));
-        if(newNode!=NULL){
-            newNode->data = data;
-            newNode->link = NULL;
-            *trav = newNode;
-            printf("\nSuccessful insertion of %d.\n", data);
-        }else{
-            printf("\nError in malloc, failed to insert %d.\n", data);
-        }
+    for(trav=S; *trav!=NULL; trav=&(*trav)->link){}
+    nodePtr newNode = (nodePtr) malloc (sizeof(struct node));
+    if(newNode!=NULL){
+        newNode->data = data;
+        newNode->link = NULL;
+        *trav = newNode;
+        printf("\nSuccessful insertion of %d.\n", data);
+    }else{
+        printf("\nError in malloc, failed to insert %d.\n", data);
     }
 }
 
@@ -105,6 +130,8 @@ void popElem(stack* S, int data){
             *trav = (*trav)->link;
             printf("\nSuccessful popping of %d.\n", data);
             free(temp); 
+        }else{
+            printf("\nElement does not exist in the stack.\n");
         }
     }else{
         printf("\nStack is empty: nothing to delete.\n");
