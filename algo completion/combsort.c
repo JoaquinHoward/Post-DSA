1b8eddfaa1738c2800820d605a29e84d9a29457c
JoaquinHoward <falarjoaquinhoward@gmail.com>
2026-06-22 01:06:39 +0800

    Added comb sort

---
+++ algo completion/combsort.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdbool.h>
+void display(int arr[], int size);
+void combSort(int arr[], int size);
+
+int main(void){
+    int arr[8] = {0, 7, 4, 0, 4, 1, 4, 8};
+    display(arr, 8);
+    combSort(arr, 8);
+    display(arr, 8);
+}
+
+void display(int arr[], int size){
+    printf("\n");
+    for(int i = 0; i < size; i++){
+        printf("%d ", arr[i]);
+    }
+}
+
+
+
+void combSort(int arr[], int size){
+    bool sorted = false;
+    for(int gap = size/1.3 ; gap > 1 || sorted == false; gap/=1.3){
+        if(gap < 1){
+            gap = 1;
+        }
+        sorted = true;
+        for(int i = 0; i + gap< size; i++){
+            if(arr[i] > arr[i + gap]){
+                int temp = arr[i];
+                arr[i] = arr[i + gap];
+                arr[i + gap] = temp;
+                sorted = false;
+            }
+        }
+    }
+}
+
--- algo completion/tempCodeRunnerFile.c
+++ algo completion/tempCodeRunnerFile.c
@@ -1,1 +1,5 @@
-r
+< size; i++){
+            if(arr[i] > arr[i + gap]){
+                int temp = arr[i];
+                arr[i] = arr[i + gap];
+                arr[i + gap
